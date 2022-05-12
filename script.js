4b20638115d147252434680f1f8f2fe5c9ab7c37
Luis David Uribe <luisdaviduribe@hotmail.com>
2022-05-11 20:31:12 -0400

    Cambios

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ var app = new Vue({
         numero: '',
         numeros: [],
         grupo: 5,
-        lista: []
+        lista: [],
+        combinaciones: 1,
     },
     methods: {
         copiar(item) {
@@ -18,15 +19,33 @@ var app = new Vue({
                 })
         },
         setNumber() {
-            this.numeros.push(this.numero);
-            this.numero = '';
+            if (this.numero) {
+                this.numeros.push(this.numero);
+                this.numero = '';
+            }
             this.updateData();
         },
         updateData() {
             this.lista = [];
             var conteo = 1;
             var localList = [];
-            for (let i = 0; i < this.numeros.length; i++) {
+            // for (let i = 0; i < this.numeros.length; i++) {
+            //     if (this.lista.length >= this.combinaciones) break;
+
+            //     var num = Math.floor(Math.random() * (this.numeros.length - 0)) + 0;
+
+            //     localList.push(this.numeros[num]);
+
+            //     if (conteo % this.grupo == 0) {
+            //         this.lista.push(localList);
+            //         conteo = 0;
+            //         localList = [];
+            //     }
+            //     conteo++;
+            // }
+            for (let i = 1; i <= this.combinaciones * this.grupo; i++) {
+                //if (this.lista.length >= this.combinaciones) break;
+
                 var num = Math.floor(Math.random() * (this.numeros.length - 0)) + 0;
 
                 localList.push(this.numeros[num]);
